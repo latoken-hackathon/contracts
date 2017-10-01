d40fd32fe3b38ebd5d70cba6f3b88691708d85a4
artyrian <artyrian@yandex-team.ru>
2017-10-01 13:25:46 +0300

    add contracts TrackFactory, Track, Author, Renter

--- general.sol
+++ general.sol
@@ -1,14 +1,27 @@
-import "LAProt.sol"
 
-contract Song is IInformation {
+contract SongFactory {
+    mapping(address => Track) tracks;
     
-    address owner;
-    string songName;
-    string authorName;
+    function upload(string trackHash, string trackName) public {
+        // save to songs map
+        Author author = new Author();
+        tracks[msg.sender] = new Track(trackHash, trackName, author);
+    }
+}
+
+contract Track is IInformation {
+    
+    string public hash;
+    string name;
+    Author author;
     Price[] prices;
     
     struct Price {
         uint exludePrice; 
+    }
+    
+    function Track(string hash, string name, Author author) {
+        
     }
     
     function setField(int field, bytes vals, uint rId) 
@@ -16,6 +29,32 @@ contract Song is IInformation {
     {
         // ??? LAP
     }
+    
+    function pay() payable {
+        // use setField?    
+    }
 }
 
+contract Author is IIdentity {
+    string name;
+    string desc;
+    mapping(string => Track) tracks;
+    
+    function add(string hash, Track track) {
+        tracks[hash] = track;
+    }
+    
+    function rent(Track track) {
+        // rent
+    }
+    
+    function sell(Track track) {
+        // sell
+        // change author and track owner
+        // remove from tracks;
+    }
+}
 
+contract Renter is IIdentity {
+    
+}
