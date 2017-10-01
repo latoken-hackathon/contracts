dfaae06ee31ec12c8f2d6dc7a053c88855c31779
artyrian <artyrian@yandex-team.ru>
2017-10-01 14:24:40 +0300

    add remix total sol

--- remix.sol
+++ remix.sol
@@ -1,3 +1,23 @@
+//import "LAProt.sol";
+
+// instead of import
+// =================
+
+contract IInformation {
+    modifier onlySource(int field, bytes vals, uint rId) {
+        require(true);
+        _;
+    }
+    
+    function setField(int field, bytes vals, uint rId) 
+        onlySource(field, vals, rId) returns (bool ok);
+}
+
+contract IIdentity {
+    // author
+}
+
+// ==========================
 
 
 contract TrackFactory {
