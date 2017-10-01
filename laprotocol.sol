29cd585f326ea419d0812a86bea033c47070203d
artyrian <artyrian@yandex-team.ru>
2017-10-01 12:30:26 +0300

    add laprotocol

---
+++ laprotocol.sol
@@ -0,0 +1,14 @@
+contract IInformation {
+    modifier onlySource(int field, bytes vals, uint rId) {
+        require(true);
+        _;
+    }
+    
+    function setField(int field, bytes vals, uint rId) 
+        onlySource(field, vals, rId) returns (bool ok);
+}
+
+contract IIdentity {
+	// autho desc
+}
+
